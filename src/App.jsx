e461c44762883ae87b8c5443a11ec0d77a397d81
gopalbasak1 <gopalbasak1223@gmail.com>
2023-09-14 12:20:47 +0600

    completed bookmarks, read time, blogs

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
 import Bookmarks from './components/Bookmarks/Bookmarks'
@@ -5,14 +6,26 @@ import Header from './components/Header/Header'
 
 function App() {
  
+const [bookmarks, setBookmarks] =useState([]);
+const [readingTime, setReadingTime] = useState(0);
+
+const handleAddToBookmark = blog =>{
+  const newBookmarks = [...bookmarks, blog];
+  setBookmarks(newBookmarks);
+}
+
+const handleMarkAsRead = time =>{
+  const newReadingTime = readingTime + time;
+  setReadingTime(newReadingTime);
+}
 
   return (
     <>
      <Header></Header>
      
-     <div className='md:flex'>
-      <Blogs></Blogs>
-      <Bookmarks></Bookmarks>
+     <div className='md:flex max-w-7xl mx-auto'>
+      <Blogs handleAddToBookmark={handleAddToBookmark} handleMarkAsRead={handleMarkAsRead}></Blogs>
+      <Bookmarks bookmarks={bookmarks} readingTime={readingTime}></Bookmarks>
      </div>
     
     </>
